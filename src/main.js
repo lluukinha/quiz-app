a8b55df8c4c586210cae4664fb7ced1d0601c00d
Lucas Dias de Souza <lucas.souza@plandomino.com>
2021-02-26 17:45:53 -0300

    i18n included on application

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueI18n from 'vue-i18n';
+import i18n from './internacionalization';
 
 import './animate.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+Vue.use(VueI18n);
+
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app');
